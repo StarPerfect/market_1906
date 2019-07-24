d4288147b932114af6cb0f96da459114cd4f1cc1
Corina Allen <StarPerfect@gmail.com>
2019-07-24 10:22:47 -0600

    Iteration 3 - Sorted Item List & Total Inventory

--- lib/market.rb
+++ lib/market.rb
@@ -17,4 +17,24 @@ class Market
   def vendors_that_sell(item)
     @vendors.find_all{ |vendor| vendor.inventory.keys.include? item }
   end
-end 
+
+  def sorted_item_list
+    all_items = []
+    @vendors.each do |vendor|
+      all_items << vendor.inventory.keys
+    end
+    all_items.sort_by{ |item| item }
+    .flatten
+    .uniq
+  end
+
+  def total_inventory
+    master_inventory = Hash.new(0)
+    sorted_item_list.each do |item|
+      @vendors.each do |vendor|
+        master_inventory[item] += vendor.inventory[item]
+      end
+    end
+    master_inventory
+  end
+end
